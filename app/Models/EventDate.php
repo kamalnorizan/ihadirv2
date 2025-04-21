24d7c4882527366e743245f11f757a9729dbfa91
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-04-21 15:19:23 +0800

    make model

---
+++ app/Models/EventDate.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EventDate extends Model
+{
+    /** @use HasFactory<\Database\Factories\EventDateFactory> */
+    use HasFactory;
+}
---
+++ database/migrations/2025_04_21_070543_create_event_dates_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_dates', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('event_id')->unsigned();
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('restrict')->onUpdate('cascade');
+            $table->date('event_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_dates');
+    }
+};
