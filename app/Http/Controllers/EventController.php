d134a76f66a51213754ddc27be56d20434b95da0
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-04-24 10:47:57 +0800

    submit event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
-use App\Models\EventCategory;
+use Ramsey\Uuid\Uuid;
 use Illuminate\Http\Request;
+use App\Models\EventCategory;
+use Illuminate\Support\Facades\Auth;
 
 class EventController extends Controller
 {
@@ -27,5 +29,20 @@ class EventController extends Controller
             'pax'=>'required|integer',
             'description'=>'required',
         ]);
+
+        $event = new Event();
+        $event->uuid = Uuid::uuid4();
+        $event->title = $request->title;
+        $event->location = $request->location;
+        $event->event_category_id = $request->event_category_id;
+        $event->pax = $request->pax;
+        $event->description = $request->description;
+        $event->owner_id = Auth::user()->id;
+        $event->email = Auth::user()->email;
+        $event->save();
+
+        flash('Event created successfully')->success()->important();
+
+        return redirect()->route('events.index');
     }
 }
