1a003b87b774e8e06be57c1aeea9da5739abec3b
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2025-04-29 15:59:32 +0800

    disable sanctum

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\PassportAuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::post('/login', [PassportAuthController::class, 'login']);
 Route::post('/register', [PassportAuthController::class, 'register']);
